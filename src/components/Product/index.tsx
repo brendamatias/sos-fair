bfe04be5b32959d0c84651f025c511c81824023d
Brenda Matias <brendamatias.sobral@gmail.com>
2023-09-23 17:20:18 -0300

    refactor: product component

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,99 @@
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import classNames from 'classnames'
+
+import { Dropdown } from '@/components'
+import { Container, ProductInfo, Tag, ProductPrice } from './styles'
+import {
+  categoryBackground,
+  categoryColor,
+  categoryIcon,
+  categoryLabel,
+  measureLabel,
+} from '@/constants'
+import { Product as ProductType } from '@/types'
+import { formatPrice } from '@/utils/format'
+import ProductService from '@/services/product.service'
+
+interface ProductProps extends ProductType {
+  refresh: () => Promise<void>
+}
+
+export const Product = ({
+  _id,
+  name,
+  bought,
+  price,
+  qty,
+  measure,
+  category,
+  refresh,
+}: ProductProps) => {
+  const { id = '' } = useParams()
+
+  const deleteProduct = async (productId: string) => {
+    try {
+      await ProductService.deleteProduct(productId, id)
+
+      refresh()
+      toast.success('Produto deletado com sucesso!')
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.error?.message ||
+          'Ocorreu um erro, tente novamente',
+      )
+    }
+  }
+
+  return (
+    <Container className={classNames({ bought })}>
+      <div>
+        <ProductInfo className={classNames({ bought })}>
+          <input type="checkbox" checked={bought} />
+          <div>
+            <strong>{name}</strong>
+            <span>
+              {qty}{' '}
+              {`${measureLabel[measure]}${
+                measure !== 'kilo' && qty > 1 ? 's' : ''
+              }`}
+            </span>
+          </div>
+        </ProductInfo>
+
+        <ProductPrice className={classNames({ bought })}>
+          <strong>{formatPrice(price)}</strong>
+          <span>Valor individual</span>
+        </ProductPrice>
+
+        <ProductPrice className={classNames({ bought })}>
+          <strong>{formatPrice(price)}</strong>
+          <span>Valor total</span>
+        </ProductPrice>
+
+        <div className="left">
+          <Tag
+            style={{
+              color: categoryColor[category],
+              background: categoryBackground[category],
+            }}
+            className={classNames({ bought })}
+          >
+            {categoryIcon[category]}
+            {categoryLabel[category]}
+          </Tag>
+
+          <Dropdown
+            options={[
+              { label: 'Editar', onClick: () => {} },
+              {
+                label: 'Remover',
+                onClick: () => deleteProduct(_id),
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  )
+}
--- src/pages/FairDetails/index.tsx
+++ src/pages/FairDetails/index.tsx
@@ -2,34 +2,19 @@ import { useEffect, useState } from 'react'
 import { Plus } from 'lucide-react'
 import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import classNames from 'classnames'
 
-import { Input, QuantitySelect, CategorySelect, Dropdown } from '@/components'
-import {
-  Container,
-  Form,
-  Button,
-  ProductItem,
-  ProductInfo,
-  Tag,
-  ProductPrice,
-} from './styles'
-import {
-  categoryBackground,
-  categoryColor,
-  categoryIcon,
-  categoryLabel,
-  measureLabel,
-} from '@/constants'
-import { Fair, Product } from '@/types'
-import { formatPrice } from '@/utils/format'
+import { Input, QuantitySelect, CategorySelect } from '@/components'
+import { Container, Form, Button } from './styles'
+
+import { Fair, Product as ProductType } from '@/types'
 import FairService from '@/services/fair.service'
 import ProductService from '@/services/product.service'
+import { Product } from '@/components/Product'
 
 export const FairDetails: React.FC = () => {
   const { id = '' } = useParams()
   const [fair, setFair] = useState<Fair>()
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<ProductType[]>([])
 
   const getFair = async () => {
     try {
@@ -49,20 +34,6 @@ export const FairDetails: React.FC = () => {
       const { data } = await ProductService.getProductList(id)
 
       setProducts(data)
-    } catch (error: any) {
-      toast.error(
-        error?.response?.data?.error?.message ||
-          'Ocorreu um erro, tente novamente',
-      )
-    }
-  }
-
-  const deleteProduct = async (productId: string) => {
-    try {
-      await ProductService.deleteProduct(productId, id)
-
-      getProducts()
-      toast.success('Produto deletado com sucesso!')
     } catch (error: any) {
       toast.error(
         error?.response?.data?.error?.message ||
@@ -92,58 +63,9 @@ export const FairDetails: React.FC = () => {
       </Form>
       <ul>
         {products.map((product) => (
-          <ProductItem
-            key={product.name}
-            className={classNames({ bought: product.bought })}
-          >
-            <div>
-              <ProductInfo className={classNames({ bought: product.bought })}>
-                <input type="checkbox" checked={product.bought} />
-                <div>
-                  <strong>{product.name}</strong>
-                  <span>
-                    {product.qty}{' '}
-                    {`${measureLabel[product.measure]}${
-                      product.measure !== 'kilo' && product.qty > 1 ? 's' : ''
-                    }`}
-                  </span>
-                </div>
-              </ProductInfo>
-
-              <ProductPrice className={classNames({ bought: product.bought })}>
-                <strong>{formatPrice(product.price)}</strong>
-                <span>Valor individual</span>
-              </ProductPrice>
-
-              <ProductPrice className={classNames({ bought: product.bought })}>
-                <strong>{formatPrice(product.price)}</strong>
-                <span>Valor total</span>
-              </ProductPrice>
-
-              <div className="left">
-                <Tag
-                  style={{
-                    color: categoryColor[product.category],
-                    background: categoryBackground[product.category],
-                  }}
-                  className={classNames({ bought: product.bought })}
-                >
-                  {categoryIcon[product.category]}
-                  {categoryLabel[product.category]}
-                </Tag>
-
-                <Dropdown
-                  options={[
-                    { label: 'Editar', onClick: () => {} },
-                    {
-                      label: 'Remover',
-                      onClick: () => deleteProduct(product._id),
-                    },
-                  ]}
-                />
-              </div>
-            </div>
-          </ProductItem>
+          <li key={product.name}>
+            <Product {...product} refresh={getProducts} />
+          </li>
         ))}
       </ul>
     </Container>
