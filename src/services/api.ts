d7ba66b771312f9c95826f6a8c47f1e5fe210f4d
Brenda Matias <brendamatias.sobral@gmail.com>
2023-09-25 01:37:00 -0300

    fix: api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://help-fair-api.vercel.app/',
 })
 
 api.interceptors.request.use((config) => {
