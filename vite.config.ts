50c1ba598dadafa86db6e8845b6df06d7255100a
Brenda Matias <brendamatias.sobral@gmail.com>
2023-09-25 13:32:16 -0300

    config: install vite plugin pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import * as path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -12,5 +13,5 @@ export default defineConfig({
       },
     ],
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 })
