aaae3f4f8f9b88b0c0b1108936fdd6ca9fec623f
Brenda Matias <brendamatias.sobral@gmail.com>
2023-09-25 22:53:28 -0300

    chore: config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import * as path from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +13,36 @@ export default defineConfig({
       },
     ],
   },
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'SOS Fair',
+        short_name: 'SOS Fair',
+        description: 'TO DO',
+        theme_color: '#0C0C0D',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
